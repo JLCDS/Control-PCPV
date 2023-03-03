ba39980a66f6ac5fe1ee2770d8e639e501fcda50
JLCDS <juanlopezcastrods@gmail.com>
2023-03-03 15:12:05 -0500

    primer commit

--- src/Model/Customer.java
+++ src/Model/Customer.java
@@ -1,7 +1,21 @@
 package Model;
 
+import java.io.*;
+
 public class Customer extends Person {
+    private OutputStream outputStream;
     public Customer(int rut, String name, Address direccion, int telefono) {
         super(rut, name, direccion, telefono);
+    }
+    public void writerBufferesWiter(String texto){
+        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(outputStream));
+        try {
+            writer.write(texto);
+            writer.newLine();
+            writer.flush();
+
+        }catch (IOException e){
+
+        }
     }
 }
--- src/presenter/Main.java
+++ src/presenter/Main.java
@@ -1,7 +1,11 @@
 package presenter;
 
+import java.util.Scanner;
+import Model.*;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+    Customer customer = new Customer( 0, scanner.next(), scanner.next(), scanner.nextInt());
+    customer.writerBufferesWiter(scanner.next());
     }
 }
